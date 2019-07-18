c41d578dd68e4d44d75041e3ffbe6b7fe30374cd
broltes <broltes@qq.com>
2019-07-18 22:40:24 +0800

    release:1.0.5: Fix observe, unobserve bugs

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const getSingleIo = getSingle(() => {
   return new IntersectionObserver(entries => {
     entries.forEach(item => {
       const dom = item.target;
-      const instance = getOptionsByDom(dom);
+      const instance = getInstanceByDom(dom);
+      if (!instance) return;
 
       if (item.isIntersecting) {
         // appear
@@ -35,27 +36,31 @@ const getSingleIo = getSingle(() => {
         !instance.didDisappear &&
         !instance.didDisappearOnce
       ) {
-        unobserve(dom);
+        unobserve(instance);
       }
     });
   });
 });
 
-function observe(dom, instance) {
+function observe(instance) {
+  const dom = ReactDOM.findDOMNode(instance);
   if (dom instanceof Element) {
-    observedList.push(dom);
     observedList.push(instance);
+    observedList.push(dom);
     getSingleIo().observe(dom);
   }
 }
-function unobserve(dom) {
-  const domIndex = observedList.indexOf(dom);
-  observedList.splice(domIndex, 2);
-  getSingleIo().unobserve(dom);
+function unobserve(instance) {
+  const instanceIndex = observedList.indexOf(instance);
+  if (instanceIndex >= 0) {
+    const dom = observedList[instanceIndex + 1];
+    getSingleIo().unobserve(dom);
+    observedList.splice(instanceIndex, 2);
+  }
 }
-function getOptionsByDom(dom) {
+function getInstanceByDom(dom) {
   const domIndex = observedList.indexOf(dom);
-  return observedList[domIndex + 1];
+  return observedList[domIndex - 1];
 }
 
 /**
@@ -64,23 +69,19 @@ function getOptionsByDom(dom) {
  */
 export default function(hooks) {
   return function(Cmpt) {
-    let dom;
-
     /**
      * 通过继承的方式，而非透传 props 和 复用 UI 渲染，
      * 防止影响其它 HOC，如 MobX observer
      */
     class Enhance extends Cmpt {
       componentDidMount() {
+        observe(this);
         super.componentDidMount && super.componentDidMount.call(this);
-
-        dom = ReactDOM.findDOMNode(this);
-        observe(dom, this);
       }
 
       componentWillUnmount() {
+        unobserve(this);
         super.componentWillUnmount && super.componentWillUnmount.call(this);
-        unobserve(dom);
       }
     }
 
